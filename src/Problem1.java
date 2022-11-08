e195f0be91c9ccc91c9a9963c9902b629fba8761
Lauren Newman <laurnewman1@gmail.com>
2022-11-08 14:53:17 -0500

    prob 1 dynamic bottom up

--- src/Problem1.java
+++ src/Problem1.java
@@ -39,6 +39,34 @@ public class Problem1 {
     }
 
     public static Transaction dynamicBU(int[][] stocks) {
-        throw new UnsupportedOperationException();
+        int m = stocks.length;
+        int n = stocks[0].length;
+        int maxProfitBeforeBuy[][] = new int[m][n]; // always negative
+        int maxProfitAfterBuy[][] = new int[m][n];
+        Transaction opt = new Transaction();
+        for (int i = 0; i < m; i++) {
+            maxProfitBeforeBuy[i][0] = -1 * stocks[i][0];
+            maxProfitAfterBuy[i][0] = -1 * stocks[i][0];
+            for (int j = 1; j < n; j++) {
+                if (maxProfitBeforeBuy[i][j - 1] > -1 * stocks[i][j]) {
+                    maxProfitBeforeBuy[i][j] = maxProfitBeforeBuy[i][j - 1];
+                }
+                else { // Buy it
+                    maxProfitBeforeBuy[i][j] = -1 * stocks[i][j];
+                    opt.stock = i;
+                    opt.buyDay = j;
+
+                }
+                if (maxProfitAfterBuy[i][j - 1] > maxProfitBeforeBuy[i][j - 1] + stocks[i][j]) {
+                    maxProfitAfterBuy[i][j] = maxProfitAfterBuy[i][j - 1];
+                }
+                else { // Sell current and buy this
+                    maxProfitAfterBuy[i][j] = maxProfitBeforeBuy[i][j - 1] + stocks[i][j];
+                    opt.sellDay = j;
+                    opt.profit = maxProfitAfterBuy[i][j];
+                }
+            }
+        }
+        return opt;
     }
 }
