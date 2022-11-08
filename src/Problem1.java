3b58c2bb583f4dd09fed9c3cb4f860f5084df844
Lauren Newman <laurnewman1@gmail.com>
2022-11-08 13:45:43 -0500

    prob 1 brute

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,7 +1,19 @@
 public class Problem1 {
 
     public static Transaction bruteForce(int[][] stocks) {
-        throw new UnsupportedOperationException();
+        int m = stocks.length;
+        int n = stocks[0].length;
+        Transaction opt = new Transaction(0, 0, 0, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j1 = 0; j1 < n; j1++) {
+                for (int j2 = j1 + 1; j2 < n; j2++) {
+                    if (stocks[i][j2] - stocks[i][j1] > opt.profit) {
+                        opt = new Transaction(i, j1, j2, stocks[i][j2] - stocks[i][j1]);
+                    }
+                }
+            }
+        }
+        return opt;
     }
 
     public static Transaction greedy(int[][] stocks) {
--- src/Transaction.java
+++ src/Transaction.java
@@ -2,6 +2,7 @@ public class Transaction {
     public int stock;
     public int buyDay;
     public int sellDay;
+    public int profit;
 
     Transaction() {}
 
@@ -9,6 +10,13 @@ public class Transaction {
         this.stock = stock;
         this.buyDay = buyDay;
         this.sellDay = sellDay;
+    }
+
+    Transaction(int stock, int buyDay, int sellDay, int profit) {
+        this.stock = stock + 1;
+        this.buyDay = buyDay + 1;
+        this.sellDay = sellDay + 1;
+        this.profit = profit;
     }
 
     public void print() {
